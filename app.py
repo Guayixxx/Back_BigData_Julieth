04328dfbc7dceb131ef7aaed57e9189a7f0c9d27
Juan Pablo <juanpa.guayara@outlook.com>
2024-09-17 09:19:14 -0500

    Prueba

--- app.py
+++ app.py
@@ -64,14 +64,14 @@ def hello_world():
     return jsonify({"message": "Hola Mundo"}), 200
 
 # Ruta para obtener first_name, last_name y birth_date de todos los usuarios
-@app.route('/users', methods=['GET'])
-def get_users():
-    try:
-        users = User.query.with_entities(User.first_name, User.last_name, User.birth_date).all()
-        users_list = [{"first_name": user.first_name, "last_name": user.last_name, "birth_date": user.birth_date.strftime('%Y-%m-%d')} for user in users]
-        return jsonify(users_list), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+# @app.route('/users', methods=['GET'])
+# def get_users():
+#     try:
+#         users = User.query.with_entities(User.first_name, User.last_name, User.birth_date).all()
+#         users_list = [{"first_name": user.first_name, "last_name": user.last_name, "birth_date": user.birth_date.strftime('%Y-%m-%d')} for user in users]
+#         return jsonify(users_list), 200
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 
 # Inicia la aplicación Flask
 if __name__ == '__main__':
